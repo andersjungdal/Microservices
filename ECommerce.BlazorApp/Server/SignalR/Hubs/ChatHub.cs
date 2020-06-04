86ebd478d403b8509955fdb47867ce385c1a6b21
andersjungdal <anders.jungdal@gmail.com>
2020-06-04 12:56:35 +0200

    Signal-R videre udvikling

--- ECommerce.BlazorApp/Server/SignalR/Hubs/ChatHub.cs
+++ ECommerce.BlazorApp/Server/SignalR/Hubs/ChatHub.cs
@@ -8,26 +8,61 @@ namespace ECommerce.BlazorApp.Server.SignalR.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task JoinGroup(string groupId)
+        #region Join
+        public async Task JoinGroup1(string groupId)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
-            await Clients.Caller.SendAsync("NewUserEntered", "Hi, Welcome to group chat");
+            await Clients.Caller.SendAsync("NewUserEntered1", "Hi, Welcome to group chat");
         }
-        //Sender til ét rum
-        public async Task SendGroup(string message, string groupId)
+        public async Task JoinGroup2(string groupId)
         {
-            await Clients.Group(groupId).SendAsync("SendMessageToGroup", message);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
+            await Clients.Caller.SendAsync("NewUserEntered2", "Hi, Welcome to group chat");
         }
-        public async Task LeaveGroup(string groupId)
+        public async Task JoinGroup3(string groupId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
+            await Clients.Caller.SendAsync("NewUserEntered3", "Hi, Welcome to group chat");
+        }
+        #endregion
+
+        #region SendToGroup
+        public async Task SendGroup1(string message, string groupId)
+        {
+            await Clients.Group(groupId).SendAsync("SendMessageToGroup1", message);
+        }
+        public async Task SendGroup2(string message, string groupId)
+        {
+            await Clients.Group(groupId).SendAsync("SendMessageToGroup2", message);
+        }
+        public async Task SendGroup3(string message, string groupId)
+        {
+            await Clients.Group(groupId).SendAsync("SendMessageToGroup3", message);
+        }
+        #endregion
+
+        #region Leave
+        public async Task LeaveGroup1(string groupId)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupId);
-            await Clients.Caller.SendAsync("UserLeaved", "Goodbye user");
+            await Clients.Caller.SendAsync("UserLeaved1", "Goodbye user");
         }
-        //Sender til alle rum
-        public async Task SendMessageToAll(string message)
+        public async Task LeaveGroup2(string groupId)
         {
-            await Clients.All.SendAsync("SendMessageToGroup", message);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupId);
+            await Clients.Caller.SendAsync("UserLeaved2", "Goodbye user");
         }
+        public async Task LeaveGroup3(string groupId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupId);
+            await Clients.Caller.SendAsync("UserLeaved3", "Goodbye user");
+        }
+        #endregion
+
+        //public async Task SendMessageToAll(string message)
+        //{
+        //    await Clients.All.SendAsync("SendMessageToGroup", message);
+        //}
 
     }
 }
